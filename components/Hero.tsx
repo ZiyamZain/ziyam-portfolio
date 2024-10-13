7b765bec7e94866626ecabbdfa5580708904f454
ZiyamGH <122663843+ZiyamZain@users.noreply.github.com>
2024-10-14 00:35:55 +0530

    Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton';
+
+const Hero = ({
+  title,icon,position,handleClick,otherClasses
+} : {
+  title:string 
+}) => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen "
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw] "
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05 ] bg-grid-black/[0.3] absolute top-0 left-0 flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      </div>
+      <div className="flex justify-center relative my-20">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seemless Experiences"
+          />
+          <p className="text-center md:tracking-wider mb:4 text-sm md:text-lg lg:text-2xl">
+            Hi,I'm Ziyam,a FullStackDeveloper Based in India
+          </p>
+          <a href="#about">
+            <MagicButton
+             title="show my work"/>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero
