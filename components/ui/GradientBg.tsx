d4ae277ef365635801e7998cc61eabd873bcc148
ZiyamGH <122663843+ZiyamZain@users.noreply.github.com>
2024-10-18 23:13:22 +0530

    completed hero section and modified something

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import { cn } from "@/utils/cn";
 import { useEffect, useRef, useState } from "react";
 
@@ -89,7 +89,7 @@ export const BackgroundGradientAnimation = ({
   return (
     <div
       className={cn(
-        "h-screen w-screen relative overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
+        "h-full w-full absolute overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
         containerClassName
       )}
     >
